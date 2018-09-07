4222273c905a173c7dedbe2b0b65be7df7b71414
shiro <shiro.mns@gmail.com>
2018-09-08 04:03:40 +0900

    [リファクタリング]Viewで処理していたループ・分岐処理をHelperに移行

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,35 @@ module ApplicationHelper
             link_to " 過去結果", "http://ykamiya.ciao.jp/result"+result_no_text+"/result_chara/result_Eno"+file_name+".html", :target => "_blank"
         end
     end
+    
+    def render_subjects(object, subjects, params, open, marked)
+        subjects.each do |subject, subject_name|
+            render_subject_td(object, subject, params, open, marked)
+        end
+    end
+
+    def render_subject_td(object, subject, params, open, marked)
+        if params["min_0_hidden"] && open[subject] == 0
+        else
+          if params["min_0_gray"] && open[subject] == 0 then
+            haml_tag :td, style: "background-color: #aaa;" do
+              render_subject(object, subject, params, open)
+            end
+          elsif marked && marked[subject] == "on" then
+            haml_tag :td, style: "background-color: #faa;" do
+              render_subject(object, subject, params, open)
+            end
+          else
+            haml_tag :td do
+              render_subject(object, subject, params, open)
+            end
+          end
+        end
+    end
+
+    def render_subject(object, subject, params, open)
+        if object
+            haml_concat object[subject]
+        end
+    end
 end
