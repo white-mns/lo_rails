3a0f6fe5f075700068bdb871279afd4dce868542
shiro <shiro.mns@gmail.com>
2018-09-10 15:20:56 +0900

    [追加]scaffoldで作成したファイルを調整

--- app/controllers/card_use_pages_controller.rb
+++ app/controllers/card_use_pages_controller.rb
@@ -5,8 +5,8 @@ class CardUsePagesController < ApplicationController
   # GET /card_use_pages
   def index
     param_set
-    @count	= CardUsePage.includes(:p_name).search(params[:q]).result.count()
-    @search	= CardUsePage.includes(:p_name).page(params[:page]).search(params[:q])
+    @count	= CardUsePage.search(params[:q]).result.count()
+    @search	= CardUsePage.page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
     @card_use_pages	= @search.result.per(50)
   end
@@ -22,16 +22,35 @@ class CardUsePagesController < ApplicationController
     reference_text_assign(params, "p_name_name", "p_name_form")
     reference_number_assign(params, "result_no", "result_no_form")
     reference_number_assign(params, "generate_no", "generate_no_form")
-    reference_number_assign(params, "battle_page", "battle_page_form")
+    reference_text_assign(params, "battle_page", "battle_page_form")
     reference_number_assign(params, "party", "party_form")
-    reference_number_assign(params, "use_cards", "use_cards_form")
+    reference_text_assign(params, "use_cards", "use_cards_form")
     
+    if !params["is_form"]  then 
+        params["party_all"] = "on"
+    end
+    if !params["party_one"] && !params["party_all"] then 
+        params["party_one"] = "on"
+        params["party_all"] = "on"
+    end
+
+    if params["party_one"] && params["party_all"] then 
+        params[:q]["party_gteq"] = 0
+    elsif params["party_all"] then
+        params[:q]["party_eq"] = 0
+    elsif params["party_one"] then
+        params[:q]["party_gteq"] = 1
+    end
+
     @p_name_form = params["p_name_form"]
     @result_no_form = params["result_no_form"]
     @generate_no_form = params["generate_no_form"]
     @battle_page_form = params["battle_page_form"]
     @party_form = params["party_form"]
     @use_cards_form = params["use_cards_form"]
+    
+    @party_all = params["party_all"]
+    @party_one = params["party_one"]
   end
   # GET /card_use_pages/1
   #def show
