e08dcc717b63d523fd7f4304aaf5e3327eb68853
shiro <shiro.mns@gmail.com>
2018-10-10 12:08:05 +0900

    [変更]クラスメソッドでチェーンしていた箇所をscopeに書き換え

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,16 +2,15 @@ class Training < ApplicationRecord
 	belongs_to :p_name,	       :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Name'
 	belongs_to :training_name, :foreign_key => :training, :primary_key => :proper_id, :class_name => 'ProperName'
   
-    def self.training_type(type, params)
-      self.includes(:p_name, :training_name).where(training_type: type).search(params[:q]).result
-    end
+    scope :training_type, ->(type)   { includes(:p_name, :training_name).where(training_type: type) }
+    scope :search_result, ->(params) { search(params[:q]).result }
     
-    def self.to_training_graph(column)
+    scope :to_training_graph, ->(column) {
       training_name = Hash[*ProperName.pluck(:proper_id, :name).flatten]
       self.pluck(column).inject(Hash.new(0)){|hash, a| hash[training_name[a]] += 1 ; hash}.sort_by{ |k, v| v}
-    end
+    }
 
-    def self.to_training_rate_graph(column)
+    scope :to_training_rate_graph, ->(column) {
       training_name = Hash[*ProperName.pluck(:proper_id, :name).flatten]
 
       training_all_count = self.count()
@@ -21,5 +20,5 @@ class Training < ApplicationRecord
           training_hash[key] = (value.to_f * 100 / training_all_count.to_f).floor(1)
       }
       training_hash.sort_by{ |k, v| v}
-    end
+    }
 end
