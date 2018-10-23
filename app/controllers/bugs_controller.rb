39e4bd0d95e5ee8db1d6f202e7fbe7462084cc07
shiro <shiro.mns@gmail.com>
2018-10-23 14:39:39 +0900

    [追加]scaffoldでBUG出現一覧を作成、migrationでindex追加

---
+++ app/controllers/bugs_controller.rb
@@ -0,0 +1,85 @@
+class BugsController < ApplicationController
+  include MyUtility
+  before_action :set_bug, only: [:show, :edit, :update, :destroy]
+
+  # GET /bugs
+  def index
+    param_set
+    @count	= Bug.notnil().includes(:p_name).search(params[:q]).result.count()
+    @search	= Bug.notnil().includes(:p_name).page(params[:page]).search(params[:q])
+    @search.sorts = 'id asc' if @search.sorts.empty?
+    @bugs	= @search.result.per(50)
+  end
+
+  def param_set
+    @last_result = Name.maximum('result_no')
+    
+    params[:q] ||= {}
+    if !params["is_form"] then
+        params["result_no_form"] ||= sprintf('%d',@last_result)
+    end
+    
+    reference_text_assign(params, "p_name_name", "p_name_form")
+    reference_number_assign(params, "result_no", "result_no_form")
+    reference_number_assign(params, "generate_no", "generate_no_form")
+    reference_number_assign(params, "e_no", "e_no_form")
+    reference_number_assign(params, "bug_e_no", "bug_e_no_form")
+    reference_number_assign(params, "lv", "lv_form")
+    
+    @p_name_form = params["p_name_form"]
+    @result_no_form = params["result_no_form"]
+    @generate_no_form = params["generate_no_form"]
+    @e_no_form = params["e_no_form"]
+    @bug_e_no_form = params["bug_e_no_form"]
+    @lv_form = params["lv_form"]
+  end
+  # GET /bugs/1
+  #def show
+  #end
+
+  # GET /bugs/new
+  #def new
+  #  @bug = Bug.new
+  #end
+
+  # GET /bugs/1/edit
+  #def edit
+  #end
+
+  # POST /bugs
+  #def create
+  #  @bug = Bug.new(bug_params)
+
+  #  if @bug.save
+  #    redirect_to @bug, notice: 'Bug was successfully created.'
+  #  else
+  #    render action: 'new'
+  #  end
+  #end
+
+  # PATCH/PUT /bugs/1
+  #def update
+  #  if @bug.update(bug_params)
+  #    redirect_to @bug, notice: 'Bug was successfully updated.'
+  #  else
+  #    render action: 'edit'
+  #  end
+  #end
+
+  # DELETE /bugs/1
+  #def destroy
+  #  @bug.destroy
+  #  redirect_to bugs_url, notice: 'Bug was successfully destroyed.'
+  #end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_bug
+      @bug = Bug.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def bug_params
+      params.require(:bug).permit(:result_no, :generate_no, :e_no, :bug_e_no, :lv)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_065925) do
+ActiveRecord::Schema.define(version: 2018_10_23_050453) do
+
+  create_table "bugs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "result_no"
+    t.integer "generate_no"
+    t.integer "e_no"
+    t.integer "bug_e_no"
+    t.integer "lv"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bug_e_no"], name: "index_bugs_on_bug_e_no"
+    t.index ["e_no", "result_no", "generate_no"], name: "resultno_eno"
+    t.index ["lv"], name: "index_bugs_on_lv"
+  end
 
   create_table "card_data", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "card_id"
