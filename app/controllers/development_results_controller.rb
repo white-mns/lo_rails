1fdf29ec60c07e193ff6737d7492686bd3cf1327
shiro <shiro.mns@gmail.com>
2018-11-28 08:23:17 +0900

    [追加]表示項目に応じてソート優先順を変更

--- app/controllers/development_results_controller.rb
+++ app/controllers/development_results_controller.rb
@@ -26,7 +26,7 @@ class DevelopmentResultsController < ApplicationController
                                     count(development_result = 1 or null) / count(*) AS win_per,
                                     min(places.result_no) AS min_result_no, max(places.result_no) AS max_result_no")
                                 .search(params[:q])
-    @search.sorts = 'id asc' if @search.sorts.empty?
+    @search.sorts = @sort if @search.sorts.empty?
     @development_results	= @search.result
     
     @all = DevelopmentResult.notnil().includes(:p_name).joins(:place, :parameter_control)
