77c8a9aad3add1b84d057c98363a8597c50262fa
shiro <shiro.mns@gmail.com>
2018-12-31 02:02:38 +0900

    [追加]placeholder入力用の共通変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def placeholder_set
+        @placeholder = {}
+        @placeholder["Number"] = "例）1~10/50~100"
+        @placeholder["Text"]   = "例）カード/\"無名のカード\""
+        @placeholder["Name"]   = "例）太郎/\"次郎\""
+        @placeholder["Card"]   = "例）斬払/\"傷突刺\" -列"
+    end
 end
